843482ce8c0708a2cd26a7a66376dc604f5a9c48
DenPerek <denperek@gmail.com>
2020-03-05 15:16:12 +0200

    my first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+'use strict'
+
+let start = document.querySelector('#start');
+
+let budgetValue =  document.querySelector('.budget-value');
+let daybudgetValue = document.querySelector('.daybudget-value');
+let levelValue = document.querySelector('.level-value');
+let expensesValue = document.querySelector('.expenses-value');
+let optionalexpensesValue = document.querySelector('.optionalexpenses-value');
+let incomeValue = document.querySelector('.income-value');
+let monthsavingsValue = document.querySelector('.monthsavings-value');
+let yearsavingsValue = document.querySelector('.yearsavings-value');
+
+let expensesItems = document.querySelectorAll('.expenses-item');
+
+let buttons = document.getElementsByTagName('button');
+let expensesItemBtn = buttons[0];
+let optionalexpensesBtn = buttons[1];
+let countBudgetBtn = buttons[2];
+
+let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
+
+let income = document.querySelector('#income');
+let checksavings = document.querySelector('#savings');
+let sum = document.querySelector('#sum');
+let percent = document.querySelector('#percent');
+
+let yearValue = document.querySelector('.year-value');
+let monthValue = document.querySelector('.month-value');
+let dayValue = document.querySelector('.day-value');
+
+document.querySelector('.choose-expenses').style.fontSize = '180px';
+
